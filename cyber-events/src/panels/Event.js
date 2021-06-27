b3e2a08e278b3b7b45a426d456a9d358ffe93fea
AndreSmrnv <tehmir@bk.ru>
2021-06-27 12:12:06 +0300

    add currEvent to context

--- cyber-events/src/panels/Event.js
+++ cyber-events/src/panels/Event.js
@@ -2,20 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
+import { HOME } from '../services/constants/initViews';
+import { EventsContext } from '../services/contexts/eventsContext';
 
 import persik from '../img/persik.png';
 import './Persik.css';
 
-const Event = props => (
-	<Panel id={props.id}>
-		<PanelHeader
-			left={<PanelHeaderBack onClick={props.go} data-to="home"/>}
-		>
-			Persik
-		</PanelHeader>
-		<img className="Persik" src={persik} alt="Persik The Cat"/>
-	</Panel>
-);
+const Event = props => {
+	const { events, setEvents } = useContext(EventsContext);
+	
+	return (
+		<Panel id={props.id}>
+			<PanelHeader
+				left={<PanelHeaderBack onClick={props.go} data-to={HOME} />}
+			>
+				Турнир { }
+			</PanelHeader>
+			<img className="Persik" src={persik} alt="Persik The Cat" />
+		</Panel>
+	)
+};
 
 Event.propTypes = {
 	id: PropTypes.string.isRequired,
