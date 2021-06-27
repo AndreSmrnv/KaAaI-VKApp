69d982bf87f31a5d30cd9f14d5d81dc0bee3d186
AndreSmrnv <tehmir@bk.ru>
2021-06-27 14:05:45 +0300

    event info

--- cyber-events/src/panels/Event.js
+++ cyber-events/src/panels/Event.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
+import { Panel, PanelHeader, PanelHeaderBack, Banner, Group, Button, Header, SimpleCell, InfoRow, RichCell } from '@vkontakte/vkui';
 import { HOME } from '../services/constants/initViews';
 import { EventsContext } from '../services/contexts/eventsContext';
-
+import imageUrl from '../img/card_img1.svg';
 import persik from '../img/persik.png';
 import './Persik.css';
 
@@ -16,9 +16,47 @@ const Event = props => {
 			<PanelHeader
 				left={<PanelHeaderBack onClick={props.go} data-to={HOME} />}
 			>
-				Турнир {events.currEvent.name}
+				Турнир {events.games.find(item => item.id == events.currEvent.game_id).name}
 			</PanelHeader>
-			<img className="Persik" src={persik} alt="Persik The Cat" />
+			<Group
+				header={<Header>{events.currEvent.name}</Header>}
+				>
+				
+
+				<Banner
+					mode="image"
+					size="m"
+					header={events.currEvent.name}
+					subheader={<span>{events.currEvent.description},<br />"Москва · 27 июня"</span>}
+					background={
+					<div
+						style={{
+						backgroundColor: '#000',
+						backgroundImage: `url(${imageUrl})`,
+						backgroundPosition: 'right bottom',
+						backgroundSize: 340,
+						backgroundRepeat: 'no-repeat',
+						}}
+					/>
+					}
+					asideMode="dismiss"
+					actions={<Button mode="overlay_primary" size="m">активен</Button>}
+				/>
+			</Group>
+			<Group>
+				<SimpleCell>
+					<InfoRow header="Родной город">
+						Ереван
+					</InfoRow>
+				</SimpleCell>
+			</Group>
+			<Group>
+				<RichCell				
+					after="М"
+				>
+								Формат
+				</RichCell>
+			</Group>
 		</Panel>
 	)
 };
