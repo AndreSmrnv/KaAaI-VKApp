689eecd4d59ca145f5f932f58cbc0be416305be3
shiryaev585 <alex_sh_v@mail.ru>
2021-06-28 14:34:31 +0300

    navigation refactored

--- cyber-events/src/components/Navigation/Navigation.js
+++ cyber-events/src/components/Navigation/Navigation.js
@@ -1,70 +1,63 @@
 import React from 'react';
-import { Panel, Tabs, withAdaptivity, View, TabsItem, Group, HorizontalScroll } from '@vkontakte/vkui';
-import { Icon24NewsfeedOutline, Icon24FavoriteOutline, Icon24UserCircleOutline } from '@vkontakte/icons';
+import { Panel, Tabs, View, TabsItem, Text } from '@vkontakte/vkui';
+import {
+    Icon24NewsfeedOutline,
+    Icon24FavoriteOutline,
+    Icon24UserCircleOutline,
+} from '@vkontakte/icons';
 import classes from './Navigation.module.css';
 
-
 class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
 
-    constructor (props) {
-      super(props);
-
-      this.state = {
-        activePanel: 'panel1',
-        contextOpened: false,
-        mode: 'all',
-        activeTab1: 'events',
-        activeTab2: 'favorite',
-        activeTab3: 'user-profile',
-      };
-
-      this.select = this.select.bind(this);
+        this.state = {
+            activePanel: 'panel1',
+            activeTab1: 'events',
+            activeTab2: 'favorite',
+            activeTab3: 'user-profile',
+        };
     }
 
-    select(e) {
-      const mode = e.currentTarget.dataset.mode;
-      this.setState({ mode, contextOpened: false });
+    render() {
+        return (
+            <View activePanel={this.state.activePanel}>
+                <Panel id="panel1">
+                    <Tabs>
+                        <TabsItem
+                            onClick={() =>
+                                this.setState({ activeTab1: 'events' })
+                            }
+                            selected={this.state.activeTab1 === 'events'}
+                        >
+                            <Icon24NewsfeedOutline style={{ margin: 'auto' }} />
+                            <Text className={classes.txt}>События</Text>
+                        </TabsItem>
+                        <TabsItem
+                            onClick={() =>
+                                this.setState({ activeTab1: 'favorite' })
+                            }
+                            selected={this.state.activeTab1 === 'favorite'}
+                        >
+                            <Icon24FavoriteOutline style={{ margin: 'auto' }} />
+                            <Text className={classes.txt}>Избранное</Text>
+                        </TabsItem>
+                        <TabsItem
+                            onClick={() =>
+                                this.setState({ activeTab1: 'user-profile' })
+                            }
+                            selected={this.state.activeTab1 === 'user-profile'}
+                        >
+                            <Icon24UserCircleOutline
+                                style={{ margin: 'auto' }}
+                            />
+                            <Text className={classes.txt}>Профиль</Text>
+                        </TabsItem>
+                    </Tabs>
+                </Panel>
+            </View>
+        );
     }
+}
 
-    render () {
-      return (
-        <View activePanel={this.state.activePanel}>
-          <Panel id="panel1">
-            <Group>
-              <Tabs>
-                <HorizontalScroll>
-                  <TabsItem
-                    onClick={() => this.setState({ activeTab1: 'events' })}
-                    selected={this.state.activeTab1 === 'events'}
-                  >
-                    <Icon24NewsfeedOutline style={{margin: 'auto'}} />
-                    События
-                  </TabsItem>
-                  <TabsItem
-                    onClick={() => this.setState({ activeTab1: 'favorite' })}
-                    selected={this.state.activeTab1 === 'favorite'}
-                  >
-                    <Icon24FavoriteOutline style={{margin: 'auto'}} />
-                    Избранное
-                  </TabsItem>
-                  <TabsItem
-                    onClick={() => this.setState({ activeTab1: 'user-profile' })}
-                    selected={this.state.activeTab1 === 'user-profile'}
-                  >
-                    <Icon24UserCircleOutline style={{margin: 'auto'}} />
-                    Профиль
-                  </TabsItem>
-                </HorizontalScroll>
-              </Tabs>
-            </Group>
-          </Panel>
-        </View>
-      )
-    }
-  }
-  
-  const AdaptivityExample = withAdaptivity(Navigation, { sizeX: true });
-
-  <AdaptivityExample />
-
-  export default Navigation;
+export default Navigation;
